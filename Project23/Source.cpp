5e6749226bcda3fb8bd1b7741a9e39ef046b5294
PANCHITA WALLATRI <63010666@KMITL.AC.TH>
2020-11-08 16:10:20 +0700

    Add project files.

---
+++ Project23/Source.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<windows.h>
+#include<time.h>
+void shuffle(int* array) {
+	for (int i = 0; i < 5; i++) {
+		int j = rand() % (i + 1);
+		int t = array[i];
+		array[i] = array[j];
+		array[j] = t;
+	}
+}
+int main() {
+	srand(time(nullptr));
+	int a[6];
+	for (int i = 0; i < 5; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	if (*a > 0 && *a != NULL)
+	{
+		shuffle(a);
+	}
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
